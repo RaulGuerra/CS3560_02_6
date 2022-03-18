11ae1fb0e64e9f8b89e597a15d93cbae7068b8b5
RaulGuerra <89431126+RaulGuerra@users.noreply.github.com>
2022-03-17 23:56:58 -0700

    Created skeleton for Food class

---
+++ Food.java
@@ -0,0 +1,47 @@
+public class Food {
+
+    //constructor
+    public void Food()
+    {}
+
+    //methods
+    public int getFoodID(String foodName)
+    {
+        //create query: 
+        return 0;
+    }
+
+    public String getFoodinfo(int foodID)
+    {
+        //create query: 
+        return 0;
+    }
+
+    public void addFoodItem()
+    {
+        //ask for the name
+        //price
+        //calorie
+        //....
+
+        //create a query: SELECT food ADD (name, price, calories)
+    }
+
+    public void removeFoodItem(int foodID)
+    {
+        //ask for the name
+        //price
+        //calorie
+        //....
+
+        //create a query: SELECT food REMOVE (foodID)
+    }
+
+    public void modifyFoodItem(int foodID)
+    {
+        //ask user which attribute to modify
+        //modify
+    }
+
+
+}
