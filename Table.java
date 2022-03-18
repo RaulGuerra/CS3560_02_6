e93ec330777159bc6549f899ed99382558c7b6e4
Isaac1113 <89550568+Isaac1113@users.noreply.github.com>
2022-03-18 00:06:56 -0700

    Add Table skeleton class

---
+++ Table.java
@@ -0,0 +1,67 @@
+
+public class Table {
+	
+	private int tableNum;
+	private int seats;
+	private boolean isOccupied;
+	private int xCoord;
+	private int yCoord;
+	
+	// Constructor maybe
+	public void Table() {
+		
+	}
+	
+	// Getters:
+	// returns the table number of this table
+	public int getTableNum() {
+		return tableNum;
+	}
+	
+	// returns the number of seats at this table
+	public int getSeats() {
+		return seats;
+	}
+	
+	// returns whether the table is occupied
+	public boolean getOccupied() {
+		return isOccupied;
+	}
+	
+	// returns the x-coordinate of where the table is on the UI
+	public int getXCoord() {
+		return xCoord;
+	}
+	
+	// returns the y-coordinate of where the table is on the UI
+	public int getYCoord() {
+		return yCoord;
+	}
+	
+	// Setters:
+	
+	// set the table number
+	public void setTableNum(int tableNum) {
+		
+	}
+	
+	// set the number of seats at the table
+	public void setSeats(int seatNum) {
+		
+	}
+	
+	// sets whether the table is occupied
+	public void setOccupied(boolean occupied) {
+		
+	}
+	
+	// set the x-coordinate of where the table is on the UI
+	public void setXCoord(int x) {
+		
+	}
+	
+	// set the y-coordinate of where the table is on the UI
+	public void setYCoord(int y) {
+		
+	}
+}
