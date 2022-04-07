641cb62d903b93f1daef34ca50bc19992804523a
RaulGuerra <89431126+RaulGuerra@users.noreply.github.com>
2022-04-07 16:40:37 -0700

    Main file to test database connection with java.
    Runs connection command and small SELECT query for the food table, which should be already in the database. Using port 3306.

---
+++ Main.java
@@ -0,0 +1,68 @@
+package posSystem;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		selectFoodTable();
+	}
+	
+	//*****************************************
+
+	
+	public static Connection getConnection() throws Exception{
+		  try{
+		   String driver = "com.mysql.cj.jdbc.Driver";
+		   String url = "jdbc:mysql://localhost:3306/posdb";
+		   String username = "root";
+		   String password = "DBpassword1";
+		   Class.forName(driver);
+		   
+		   Connection conn = DriverManager.getConnection(url,username,password);
+		   System.out.println("Sucessfully connected to database...\n");
+		   return conn;
+		  } catch(Exception e){System.out.println(e);}
+		  
+		  
+		  return null;
+		 }
+	
+	//*****************************************
+	
+	public static ArrayList<String> selectFoodTable() throws Exception{
+		try {
+			Connection con = getConnection();
+			PreparedStatement selectFood = con.prepareStatement("SELECT * FROM food;");
+			
+			ResultSet result = selectFood.executeQuery();
+			
+			ArrayList<String> array = new ArrayList<String>();
+			while(result.next()) {
+				System.out.println(result.getString("foodID"));
+				System.out.println(" ");
+				System.out.println(result.getString("name"));
+				System.out.println(" ");
+				System.out.println(result.getString("price"));
+				System.out.println(" ");
+				System.out.println(result.getString("calories"));
+				System.out.println(" ");
+				System.out.println(result.getString("description"));
+				
+				array.add(result.getString("name"));
+			}
+				System.out.println("All records have been selected!");
+				return array;
+			
+		}catch(Exception e){System.out.println(e);}
+		return null;
+	}
+
+	
+//end of main
+}
